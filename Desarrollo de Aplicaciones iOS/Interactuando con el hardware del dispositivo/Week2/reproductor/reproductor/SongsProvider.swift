e263e3448dac08a76cc7b8488b1ffe2c46e7505c
foskon <foskon@gmail.com>
2017-03-26 12:13:33 +0200

    Add SongsProvider.

---
+++ Desarrollo de Aplicaciones iOS/Interactuando con el hardware del dispositivo/Week2/reproductor/reproductor/SongsProvider.swift
@@ -0,0 +1,37 @@
+//
+//  SongsProvider.swift
+//  reproductor
+//
+//  Created by Carlos Manzanas on 26/03/17.
+//  Copyright © 2017 foskon. All rights reserved.
+//
+
+import Foundation
+
+class SongsProvider {
+    
+    private let songs: [Song]
+    
+    init() {
+        // Create songs
+        guard
+            let song1Data = Bundle.main.path(forResource: "song1", ofType: "mp3"),
+            let song2Data = Bundle.main.path(forResource: "song2", ofType: "mp3"),
+            let song3Data = Bundle.main.path(forResource: "song3", ofType: "mp3"),
+            let song4Data = Bundle.main.path(forResource: "song4", ofType: "mp3") else {
+                print("ERROR - Could not load data")
+                fatalError()
+        }
+            
+        let song1 = Song(name: "Super String Theory (ID 474)", path: song1Data)
+        let song2 = Song(name: "Doctor's In", path: song2Data)
+        let song3 = Song(name: "Lay Down", path: song3Data)
+        let song4 = Song(name: "Alice", path: song4Data)
+        
+        self.songs = [song1, song2, song3, song4]
+    }
+    
+    func getSongs() -> [Song] {
+        return songs
+    }
+}
