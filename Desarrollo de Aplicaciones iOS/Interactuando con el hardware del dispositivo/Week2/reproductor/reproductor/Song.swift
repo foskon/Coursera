1cce2b0c97edd2cc51d9c776cb871a18279d4b9b
foskon <foskon@gmail.com>
2017-03-26 11:58:04 +0200

    Add song model.

---
+++ Desarrollo de Aplicaciones iOS/Interactuando con el hardware del dispositivo/Week2/reproductor/reproductor/Song.swift
@@ -0,0 +1,20 @@
+//
+//  Song.swift
+//  reproductor
+//
+//  Created by Carlos Manzanas on 26/03/17.
+//  Copyright © 2017 foskon. All rights reserved.
+//
+
+import Foundation
+
+class Song {
+    
+    private let name: String
+    private let path: String
+    
+    init(name: String, path: String) {
+        self.name = name
+        self.path = path
+    }
+}
