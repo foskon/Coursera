a7de000975b12fefe9e76197035b5cdfc05f0478
foskon <foskon@gmail.com>
2017-03-26 11:15:56 +0200

    Add project

---
+++ Desarrollo de Aplicaciones iOS/Interactuando con el hardware del dispositivo/Week2/reproductor/reproductor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reproductor
+//
+//  Created by Carlos Manzanas on 26/03/17.
+//  Copyright © 2017 foskon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
